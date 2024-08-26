4c04524ff27a573363b8acf33c3f3473be160f36
Advaita Sharma <advaitasharma2003@gmail.com>
2024-08-27 01:32:01 +0530

    Final Commit

--- Assets/Script/DoofusControl.cs
+++ Assets/Script/DoofusControl.cs
@@ -3,9 +3,7 @@ using UnityEngine;
 public class Doofus : MonoBehaviour
 {
     public float speed = 3.0f;
-
     private Rigidbody rb;
-
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -17,6 +15,8 @@ public class Doofus : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
+        transform.Translate(Vector3.forward*Time.deltaTime * verticalInput);
+        transform.Translate(-Vector3.forward*Time.deltaTime * horizontalInput);
 
         rb.AddForce(movement * speed);
     }
